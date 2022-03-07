7da88c34c506c788c8bc69484ea23c92f0e50807
PraveenVBongale <praveen.bongale98@gmail.com>
2022-03-07 09:30:15 +0530

    Initialize Board

---
+++ TikTokGame/TikTokGame/TikTokGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TikTokGame
+{
+    public static class TikTokGame
+    {
+        private static char[] board = new char[10];
+
+        static TikTokGame()
+        {
+            Array.Fill(board, ' ');
+        }
+
+        public static void DisplayBoard()
+        {
+            for (int i = 1; i < board.Length; i++)
+            {
+                Console.Write(board[i] + " ");
+                if (i % 3 == 0)
+                    Console.WriteLine();
+            }
+        }
+    }
+}
